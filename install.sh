d4a18ec5bfe8a37a589da2823bc5b95b21247e4f
yeluoge26 <57170445+yeluoge26@users.noreply.github.com>
2019-11-05 18:25:20 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+yum install python3
+yum install vim
+cd /
+mkdir www
+cd www
+mkdir wwwroot
+cd wwwroot
+git clone https://github.com/yeluoge26/phyoutu.git
+cd pyyoutu
+pip3 install -r requirements.txt
