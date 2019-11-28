ab41e5e61ce2878171b0957a42330c8e8d0c6c82
yeluoge26 <57170445+yeluoge26@users.noreply.github.com>
2019-11-28 14:31:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 yum install python3
 yum install vim
-cd /
-mkdir www
-cd www
-mkdir wwwroot
-cd wwwroot
-git clone https://github.com/yeluoge26/phyoutu.git
-cd pyyoutu
+cd /root
+git clone https://github.com/yeluoge26/sptwitter.git
+cd sptwitter
+tar -zxvf twitter.tgz
 pip3 install -r requirements.txt
+sudo yum -y install epel-release
+sudo rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
+sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm
+yum repolist
+sudo  yum -y install ffmpeg ffmpeg-devel
+ffmpeg -version
